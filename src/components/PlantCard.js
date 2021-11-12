9d4e327e83cd2e768bd127ba2112f017fa564fc8
Dayeane <dayeane85@gmail.com>
2021-11-11 20:14:25 -0800

    step 3 dayis

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 
 function PlantCard({plant}) {
+  const [available, setAvailable] = useState(true)
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {available ? (
+        <button onClick={()=> setAvailable(!available)} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={()=> setAvailable(!available)}>Out of Stock</button>
       )}
     </li>
   );
